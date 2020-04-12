11c86b6b57f521258eb8815bba35d0669dd6c779
InsafHW <fororiginmylo@yandex.ru>
2020-04-12 10:24:57 +0300

    updates

--- lw7/7.1/js/script.js
+++ lw7/7.1/js/script.js
@@ -1,41 +1,50 @@
-function isPrimeNumber(digit){
-	if ((typeof(digit) === 'number') || (typeof(digit) === 'object')){
-		if (typeof(digit) === 'number'){
-		let isPrime = true;
-		for (let j = 2; j < digit; j++){
-			isPrime = true;
-			if ((digit % j) == 0){
-				isPrime = false;
-				break
-			}
+function isPrimeDigit(digit){
+	if (typeof(digit) == 'number'){
+		let isPrime
+		if (digit < 2){
+			console.log(digit + ' непростое число');
 		}
-		if (isPrime){
-			console.log('Число ' + digit + ' простое')
-		} else{
-			console.log('Число ' + digit + ' непростое')
-		}
-	}
-	else if (typeof(digit) === 'object'){
-		for(let i = 0; i < (digit.length); i++){
-			var num = digit[i];
-			let isPrime = true;
-			for (let j = 2; j < num; j ++){
+		else{
+			for (let i = 2; i < digit; i++){
 				isPrime = true;
-				if ((num % j) === 0){
+				if ((digit % i) == 0){
 					isPrime = false;
-					break
+					break;
 				}
 			}
 			if (isPrime){
-				console.log('Число ' + num + ' простое')
-			} else{
-				console.log('Число ' + num + ' непростое')
+				console.log(digit + ' простое число');
+			}
+			else{
+				console.log(digit + ' непростое число');
 			}
 		}
 	}
-	} else{
+	else if (typeof(digit) == 'object'){
+		for(let i = 0; i < digit.length; i++){
+			let num = digit[i];
+			let isPrime = true;
+			if (num < 2){
+				console.log(num + ' непростое число')
+			}
+			else{
+				for (let i = 2; i < num; i++){
+					isPrime = true;
+					if ((num % i) == 0){
+						isPrime = false;
+						break;
+					}
+				}
+				if (isPrime){
+					console.log(num + ' простое число');
+				}
+				else{
+					console.log(num + ' непростое число');
+				}
+			}
+		}
+	}
+	else{
 		console.log('Неккоректный ввод')
 	}
-
 }
-
