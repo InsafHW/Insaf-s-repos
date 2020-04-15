2d3f66de6116bbfff589520ba6838a012badfbb5
InsafHW <fororiginmylo@yandex.ru>
2020-04-15 16:11:56 +0300

    Updates!

--- lw7/7.1/js/script.js
+++ lw7/7.1/js/script.js
@@ -1,50 +1,50 @@
 function isPrimeDigit(digit){
-	if (typeof(digit) == 'number'){
-		let isPrime
-		if (digit < 2){
-			console.log(digit + ' непростое число');
-		}
-		else{
-			for (let i = 2; i < digit; i++){
-				isPrime = true;
-				if ((digit % i) == 0){
-					isPrime = false;
-					break;
-				}
-			}
-			if (isPrime){
-				console.log(digit + ' простое число');
-			}
-			else{
-				console.log(digit + ' непростое число');
-			}
-		}
-	}
-	else if (typeof(digit) == 'object'){
-		for(let i = 0; i < digit.length; i++){
-			let num = digit[i];
-			let isPrime = true;
-			if (num < 2){
-				console.log(num + ' непростое число')
-			}
-			else{
-				for (let i = 2; i < num; i++){
-					isPrime = true;
-					if ((num % i) == 0){
-						isPrime = false;
-						break;
-					}
-				}
-				if (isPrime){
-					console.log(num + ' простое число');
-				}
-				else{
-					console.log(num + ' непростое число');
-				}
-			}
-		}
-	}
-	else{
-		console.log('Неккоректный ввод')
-	}
+  if (typeof(digit) == 'number'){
+    let isPrime
+    if (digit < 2){
+      console.log(digit + ' непростое число');
+    }
+    else{
+      for (let i = 2; i < digit; i++){
+        isPrime = true;
+        if ((digit % i) == 0){
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime){
+        console.log(digit + ' простое число');
+      }
+      else{
+        console.log(digit + ' непростое число');
+      }
+    }
+  }
+  else if (typeof(digit) == 'object'){
+    for(let i = 0; i < digit.length; i++){
+      let num = digit[i];
+      let isPrime = true;
+      if (num < 2){
+        console.log(num + ' непростое число')
+      }
+      else{
+        for (let i = 2; i < num; i++){
+          isPrime = true;
+          if ((num % i) == 0){
+            isPrime = false;
+            break;
+          }
+        }
+        if (isPrime){
+          console.log(num + ' простое число');
+        }
+        else{
+          console.log(num + ' непростое число');
+        }
+      }
+    }
+  }
+  else{
+    console.log('Неккоректный ввод')
+  }
 }
