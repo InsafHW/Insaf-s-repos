60184ffd9378862b829fde7f0fae8e2c919050dc
InsafHW <fororiginmylo@yandex.ru>
2020-05-23 20:57:46 +0300

    Fixed

--- lw8/js/script.js
+++ lw8/js/script.js
@@ -1,15 +1,18 @@
-function slide(e) {
-	const slider = document.getElementsByClassName('content_films_info')[0];
-	let transition_elem;
-	if(e.target.id === 'leftbtn') {
-		transition_elem = slider.removeChild(slider.firstElementChild);
-		slider.append(transition_elem);
-	} else if (e.target.id === 'rightbtn') {
-		transition_elem = slider.removeChild(slider.lastElementChild);
-		slider.prepend(transition_elem);
+const leftBtn = document.getElementById	("leftbtn");
+const rightBtn = document.getElementById("rightbtn");
+
+function slide(event) {
+	const slider = document.getElementsByClassName("content_films_info")[0];
+	let transitionElem;
+	console.log(event.target.id);
+	if(event.target.id === "leftbtn") {
+		transitionElem = slider.removeChild(slider.firstElementChild);
+		slider.append(transitionElem);
+	} else if (event.target.id === "rightbtn") {
+		transitionElem = slider.removeChild(slider.lastElementChild);
+		slider.prepend(transitionElem);
 	}
 }
-const leftbtn = document.getElementById	("leftbtn");
-const rightbtn = document.getElementById('rightbtn');
-leftbtn.addEventListener('click', slide);
-rightbtn.addEventListener('click', slide);
+
+leftBtn.addEventListener('click', slide);
+rightBtn.addEventListener('click', slide);
