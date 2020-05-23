319ce96bd1c8cce16cc0a3d0e9bd23b60f96e604
InsafHW <fororiginmylo@yandex.ru>
2020-05-23 21:08:31 +0300

    fixed

--- lw8/js/script.js
+++ lw8/js/script.js
@@ -1,13 +1,15 @@
 const leftBtn = document.getElementById	("leftbtn");
 const rightBtn = document.getElementById("rightbtn");
+const leftBtnId = leftBtn.id;
+const rightBtnId = rightBtn.id;
 
 function slide(event) {
 	const slider = document.getElementsByClassName("content_films_info")[0];
 	let transitionElem;
-	if(this.id === "leftbtn") {
+	if(this.id === leftBtnId) {
 		transitionElem = slider.removeChild(slider.firstElementChild);
 		slider.append(transitionElem);
-	} else if (this.id === "rightbtn") {
+	} else if (this.id === rightBtnId) {
 		transitionElem = slider.removeChild(slider.lastElementChild);
 		slider.prepend(transitionElem);
 	}
